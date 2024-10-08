9de4e18bbd2e42dbdeed8e41e7c5a27378cb2029
Kaitlyn Burkholder <Kburkho@iu.edu>
2024-10-08 10:48:46 -0400

    Basics Variables for Javascript

---
+++ Week7/app.js
@@ -0,0 +1,56 @@
+// ways to create varriables
+// 1. is using var makes a variable that can be reassigned and globally referenced
+var myName="Kaitlyn";
+
+clownName="K"
+
+//2. using let makes a variable that can be reassigned values
+let myTitle="the Kangaroo Guy"; 
+
+// life event
+myTitle="the Crocodile Guy"; 
+
+//.3 using const makes a variable that cannot be reassigned values
+const ssn="123"; 
+
+ssn= 312; 
+
+// variable types
+// primitive variable types
+// 1. strings
+let strl="Blah blah blah";
+const str2="123";
+// const str2 = '123';
+let str3="440J"; 
+// starting quote type MUST match ending quote type
+const str4=`String that can be broken into multiple lines`; 
+
+//2. number
+let myAge= 20; 
+const oneLessThanZeo= -1; 
+let numberOfKidsThatHave= 2.33; 
+
+//3. Boolean are true or false
+let amIBald= true; 
+const isTheSunKindaFar = false; 
+
+// complex variables
+//4. null/undefined
+let myCar; 
+
+//5. arrays - list of stuff
+const myFavoriteColors =["pink", "yellow", "orange", "purple"]; 
+const randomStuff=[true, 1, "blah", -200]
+
+//.6 objects - variables that you need to describe
+const person = {
+    // property of the object
+    hairColor: "black"
+// methods are like functions that belong to the object
+sayMyName() {}, 
+}; 
+
+//7. function 
+function doSomething() {
+    // make your function do something
+}
