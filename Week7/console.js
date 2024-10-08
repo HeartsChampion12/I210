0869964f98d1c1308daa4aac2d270010cf028903
Kaitlyn Burkholder <Kburkho@iu.edu>
2024-10-08 11:29:20 -0400

    adding js to html file

---
+++ Week7/console.js
@@ -0,0 +1,42 @@
+console.log("hi"); 
+
+const myAge="21";
+
+console.log(myAge);
+
+let myDog = {
+    hairColor: 'green', 
+    length: "10ft 3in", 
+    teethCount: 274, 
+    attributes: ["cuddly", "soft"], 
+    name: "Sister", 
+}
+
+console.log(myDog); 
+
+console.log("Dog Name", myDog.name);
+console.log("Dog Friends:", myDog.friends); 
+
+/* create variables for the following
+    - your name
+    - favorite number
+    - whether you came to class today
+    - a monster & describe it's scary features (at least 3)
+*/ 
+
+let myName="Kaitlyn"; 
+console.log(myName); 
+
+const favNumber= 12; 
+console.log(favNumber); 
+
+let imHere = true; 
+console.log(imHere); 
+
+const monster = {
+    shadowyAura: true, 
+    colorOfSkin: "black", 
+    lampreyTeeth: true, 
+    whatItEats: ["souls", "skin"], 
+}
+console.log(monster)
