229a4941d3d2a9b441346747058649e84f296f95
Kaitlyn Burkholder <Kburkho@iu.edu>
2024-11-14 11:01:00 -0500

    adding on years

--- week12/connectfirebase.js
+++ week12/connectfirebase.js
@@ -3,6 +3,7 @@ import {app} from"./firebase.js";
 import {getFirestore,
     collection, 
     getDocs,
+    addDoc, 
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const booksListRef = document.querySelector("#booksList");
@@ -24,6 +25,21 @@ async function getBooks() {
 
     console.log(booksDocs)
 }
+
+async function addBook() {
+    const bookName = prompt("Book Name:"); 
+
+    const booksCollection = collection(db, "books"); 
+
+    const yearPublished = prompt("Year Published:");
+
+    await addDoc(booksCollection, {title: bookName, yearPublished: yearPublished}); 
+    getBooks(); 
+    
+    console.log(bookName); 
+}
+
+document.querySelector("#addBookButton").onclick = addBook; 
 
 getBooks(); 
 
